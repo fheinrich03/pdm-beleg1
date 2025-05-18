0e93f26b36f06816fe8805ea9300780e7e0d0c3b
fheinrich03 <heinrichfelix03@gmail.com>
2025-05-18 20:13:15 +0200

    fix style errors

--- src/main/scala/recursions/ProblemsLists.scala
+++ src/main/scala/recursions/ProblemsLists.scala
@@ -1,66 +1,59 @@
 package recursions
 
-import list.traits.IntList
 import list.implementation.*
+import list.traits.IntList
 
 import scala.annotation.tailrec
 
-object ProblemsLists :
+object ProblemsLists:
 
   /*
    * Use only recursions for your solutions
    */
 
   /**
-    *
-    * Given a number i that should be duplicated a number of times
-    * returns an IntList that contains the duplicated i
-    *
-    * E.x. duplicateNum(4,3)
-    * -> SinglyLinkedList(4, 4, 4, 4)
-    *
-    * @param i number to duplicate
-    * @param times number of duplicates
-    * @return List of duplicated numbers
-    */
-  def duplicateNum(i:Int, times:Int):IntList= {
+   *
+   * Given a number i that should be duplicated a number of times
+   * returns an IntList that contains the duplicated i
+   *
+   * E.x. duplicateNum(4,3)
+   * -> SinglyLinkedList(4, 4, 4, 4)
+   *
+   * @param i     number to duplicate
+   * @param times number of duplicates
+   * @return List of duplicated numbers
+   */
+  def duplicateNum(i: Int, times: Int): IntList = {
     @tailrec
-    def duplicateRecursive(i: Int, remainingTimes: Int, current: IntList): IntList = {
-      if (remainingTimes <= 0) current
-      else duplicateRecursive(i, remainingTimes - 1, Cons(i, current))
-    }
+    def duplicateRecursive(remainingTimes: Int, current: IntList): IntList = remainingTimes match
+      case n if n <= 0 => current
+      case n => duplicateRecursive(n - 1, Cons(i, current))
 
-    duplicateRecursive(i, times, Empty)
+    duplicateRecursive(times, Empty)
   }
 
   /**
-    *
-    * Given is an IntList l that contains arbitrary numbers and a predicate Function Int=>Boolean
-    * All numbers of the list that fulfill the predicate should be duplicated
-    * returns an IntList that contains all duplicated numbers that fulfill the predicate and the
-    * remaining other numbers in the same order as they occur in the origin list
-    *
-    * E.x. duplicateEqualNumbers(x=>(x % 2==0),SinglyLinkedIntList(1,4,3,5,8))
-    * -> SinglyLinkedIntList(1, 4, 4, 3, 5, 8, 8)
-    *
-    *  Use only recursions to solve the problem and none of the higher order Functions
-    * @param predicate predicate functions
-    * @param l IntList that should be processed
-    * @return IntList that contains the duplicates and all other nums
-    */
-  def duplicateNumbersFulfillingPredicate(predicate: Int=>Boolean, l:IntList): IntList= {
-    if (l.isEmpty) Empty
-    else {
-      val head = l.head
-      val processedTail = duplicateNumbersFulfillingPredicate(predicate, l.tail)
-
-      if (predicate(head)) {
-        Cons(head, Cons(head, processedTail))
-      } else {
-        Cons(head, processedTail)
-      }
-    }
-  }
+   *
+   * Given is an IntList l that contains arbitrary numbers and a predicate Function Int=>Boolean
+   * All numbers of the list that fulfill the predicate should be duplicated
+   * returns an IntList that contains all duplicated numbers that fulfill the predicate and the
+   * remaining other numbers in the same order as they occur in the origin list
+   *
+   * E.x. duplicateEqualNumbers(x=>(x % 2==0),SinglyLinkedIntList(1,4,3,5,8))
+   * -> SinglyLinkedIntList(1, 4, 4, 3, 5, 8, 8)
+   *
+   * Use only recursions to solve the problem and none of the higher order Functions
+   *
+   * @param predicate predicate functions
+   * @param l         IntList that should be processed
+   * @return IntList that contains the duplicates and all other nums
+   */
+  def duplicateNumbersFulfillingPredicate(predicate: Int => Boolean, l: IntList): IntList = l match
+    case Empty => Empty
+    case Cons(head, tail) =>
+      val processedTail = duplicateNumbersFulfillingPredicate(predicate, tail)
+      if (predicate(head)) Cons(head, Cons(head, processedTail))
+      else Cons(head, processedTail)
 
   /**
    *
@@ -72,10 +65,10 @@ object ProblemsLists :
    * -> Set(Cons(1, Cons(2, Cons(3, Empty))), Cons(1, Cons(2, Empty)), Cons(1, Cons(3, Empty)), Cons(1, Empty),
    * Cons(2, Cons(3, Empty)), Cons(2, Empty), Cons(3, Empty), Empty)
    *
-   * @param l     given list
+   * @param l given list
    * @return List of all subsets
    */
-  def combinations(l:IntList):List[IntList]= {
+  def combinations(l: IntList): List[IntList] = {
     def toScalaList(intList: IntList): List[Int] = {
       if (intList.isEmpty) Nil
       else intList.head :: toScalaList(intList.tail)
--- src/main/scala/tree/implementation/BinaryTree.scala
+++ src/main/scala/tree/implementation/BinaryTree.scala
@@ -5,100 +5,91 @@ import tree.traits.IntTree
 import scala.annotation.tailrec
 
 /**
-  * A companion object for the singly linked list.
-  * This enables creating lists list this: val list = SinglyLinkedIntList(1,2,3)
-  * which results in Cons(1,Cons(2,Cons(3,Empty))))
-  */
-object BinaryTree :
+ * A companion object for the singly linked list.
+ * This enables creating lists list this: val list = SinglyLinkedIntList(1,2,3)
+ * which results in Cons(1,Cons(2,Cons(3,Empty))))
+ */
+object BinaryTree:
 
   /** The apply function is a special function in scala.
-    * It can be invoked with SinglyLinkedIntList.apply(args) or simply SinglyLinkedIntList(args).
-    * This particular implementation of it is also a variadic function, i.e.
-    * a function which accepts one or more arguments of the same type (integers) as parameters.
-    */
+   * It can be invoked with SinglyLinkedIntList.apply(args) or simply SinglyLinkedIntList(args).
+   * This particular implementation of it is also a variadic function, i.e.
+   * a function which accepts one or more arguments of the same type (integers) as parameters.
+   */
   //inside this method xs is of type Seq[int]
   def apply(xs: Int*): BinaryTree =
 
     @tailrec
-    def treeIntialization(tree:BinaryTree, xs: Int*):BinaryTree= xs match
+    def treeIntialization(tree: BinaryTree, xs: Int*): BinaryTree = xs match
+      case Seq() => tree
+      //: _* results in the sequence being passed as multiple parameters - (1,2,3) instead of Seq[Int]{1,2,3}
+      case _ => treeIntialization(tree.insert(xs.head).asInstanceOf[BinaryTree], xs.tail *)
 
-    case Seq() => tree
-    //: _* results in the sequence being passed as multiple parameters - (1,2,3) instead of Seq[Int]{1,2,3}
-    case _ => treeIntialization(tree.insert(xs.head).asInstanceOf[BinaryTree],xs.tail*)
+    treeIntialization(Empty, xs *)
 
-    treeIntialization(Empty,xs*)
-
-abstract class BinaryTree extends IntTree :
+abstract class BinaryTree extends IntTree:
 
   /* Helper function used by the delete-operation
   *  Function finds the node with the next higher value
   *  compared to the root node of the binary tree (see tests)
   * */
-  def findSuccessor: BinaryTree = this match {
+  def findSuccessor: BinaryTree = this match
     case Empty => throw new Error("Cannot find successor in empty tree")
     case NonEmpty(_, _, right) =>
       @tailrec
-      def findMin(tree: IntTree): BinaryTree = tree match {
+      def findMin(tree: IntTree): BinaryTree = tree match
         case Empty => throw new Error("No minimum in empty tree")
         case NonEmpty(_, left, _) if left.isEmpty => tree.asInstanceOf[BinaryTree]
         case NonEmpty(_, left, _) => findMin(left)
-      }
 
       if (right.isEmpty) throw new Error("No successor available")
       else findMin(right)
-  }
 
-  override def delete(i: Int): IntTree = this match {
+
+  override def delete(i: Int): IntTree = this match
     case Empty => throw new Error(s"Element $i not found in tree")
     case NonEmpty(elem, left, right) =>
       if (i < elem) NonEmpty(elem, left.delete(i), right)
       else if (i > elem) NonEmpty(elem, left, right.delete(i))
-      else {
-        if (left.isEmpty) right
-        else if (right.isEmpty) left
-        else {
-          try {
-            val successor = findSuccessor
-            val successorValue = successor.root
-            val newRight = right.delete(successorValue)
-            NonEmpty(successorValue, left, newRight)
-          } catch {
-            case e: Error => throw new Error(s"Cannot delete node $i: $e")
-          }
-        }
-      }
-  }
+      else if (left.isEmpty) right
+      else if (right.isEmpty) left
+      else
+        NonEmpty(
+          findSuccessor.root,
+          left,
+          right.delete(findSuccessor.root)
+        )
 
-  override def map(mapFun: Int => Int): IntTree = this match {
+
+  override def map(mapFun: Int => Int): IntTree = this match
     case Empty => Empty
     case NonEmpty(elem, left, right) =>
       NonEmpty(mapFun(elem), left.map(mapFun), right.map(mapFun))
-  }
 
-  override def filter(filterFun: Int => Boolean): IntTree = this match {
+
+  override def filter(filterFun: Int => Boolean): IntTree = this match
     case Empty => Empty
     case NonEmpty(elem, left, right) =>
-      if (filterFun(elem)) {
+      if (filterFun(elem))
         NonEmpty(elem, left.filter(filterFun), right.filter(filterFun))
-      } else {
+      else
         merge(left.filter(filterFun), right.filter(filterFun))
-      }
-  }
 
-  private def merge(t1: IntTree, t2: IntTree): IntTree = (t1, t2) match {
+
+  private def merge(t1: IntTree, t2: IntTree): IntTree = (t1, t2) match
     case (Empty, t) => t
     case (t, Empty) => t
     case (_, _) =>
       t2.tree2List.foldLeft(t1)((tree, elem) => tree.insert(elem))
-  }
 
-  override def tree2List: List[Int] = this match {
+
+  override def tree2List: List[Int] = this match
     case Empty => List()
     case NonEmpty(elem, left, right) =>
       left.tree2List ++ List(elem) ++ right.tree2List
-  }
 
-  override def isBinaryTree: Boolean = this match {
+
+  override def isBinaryTree: Boolean = this match
     case Empty => true
     case NonEmpty(elem, left, right) =>
       val leftValid = left.isEmpty ||
@@ -112,4 +103,3 @@ abstract class BinaryTree extends IntTree :
           right.tree2List.forall(_ > elem))
 
       leftValid && rightValid
-  }
